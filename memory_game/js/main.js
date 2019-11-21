ab2512c3e28774c7e1183dc1b93149e58ece93df
cara <xw33@outlook.com>
2019-11-21 11:53:13 +0000

    game started!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = []; 
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped", cardOne);
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped", cardTwo);
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("you've found a match!");
+    } else {
+        alert("sorry, try again");
+    }
+}
