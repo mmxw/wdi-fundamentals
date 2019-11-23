de2af29db81c59dcfc02bfc4102c1f004a91f069
cara <xw33@outlook.com>
2019-11-23 19:09:56 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
-let cards = ["queen", "queen", "king", "king"];
-let cardsInPlay = []; 
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped", cardOne);
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped", cardTwo);
-if (cardsInPlay.length === 2){
-    if (cardsInPlay[0] === cardsInPlay[1]){
-        alert("you've found a match!");
-    } else {
-        alert("sorry, try again");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+
+var checkForMatch = function() {
+    if (cardsInPlay.length === 2){
+        if (cardsInPlay[0] === cardsInPlay[1]){
+            alert("you've found a match!");
+        } else {
+            alert("sorry, try again");
+        }
     }
 }
+
+var flipCard = function(cardID) {
+    console.log("User flipped", cards[cardID]);
+    cardsInPlay.push(cards[cardID]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
+
+
